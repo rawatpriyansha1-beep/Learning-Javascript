2e485a00eadae03e1ae25d95484456ec7038409f
Priyansha Rawat <priyansha.rawat@smartcosmos.com>
2026-02-24 15:13:49 +0530

    undefined vs not defined

---
+++ _ep6_undefinedVsNotDefiend.js
@@ -0,0 +1,10 @@
+// program to undertsnad undefined vs nit defined 
+console.log("printing a before conditional statements: ", a);
+var a;
+a = 10;
+if (a === undefined) {
+    console.log("a is undefined");
+} else {
+    console.log("a is not defined")
+}
+console.log("printing a ata the end of the program: ", a);
