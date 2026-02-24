a99f5ed9358dd677a01c740da1fcc909a4a2d32a
Priyansha Rawat <priyansha.rawat@smartcosmos.com>
2026-02-24 11:41:58 +0530

    Var varibale in javascript

---
+++ VAR.js
@@ -0,0 +1,17 @@
+//var (Old way – ES5)
+
+var name = "Priansha";
+var name = "rawat"; // This is allowed ✅
+
+name = "Yashi Rawat";
+
+console.log("print name :", name);
+
+var val = true
+
+if (val == true) {
+    var result = "value of {val} varibale is true ";
+
+}
+console.log("result variable contains: ", result) // Result variable is accessible outside the block ❌ {It can create Bugs}
+
