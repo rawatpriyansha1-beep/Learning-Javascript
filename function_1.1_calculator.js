bee07a0fcc55fc9c61e8cac33bb4905b5dfa2488
Priyansha Rawat <priyansha.rawat@smartcosmos.com>
2026-02-24 12:39:54 +0530

    updated input feild from terminal feature added

--- function_1.1_calculator.js
+++ function_1.1_calculator.js
@@ -1,25 +1,41 @@
 // simple calculator function program 
 
-let a = 8
-let b = 4
+const readline = require("readline");
 
-function add(x, y) {
-    return x + y
-}
-console.log("Addition of two Numbers:", add(a, b)); // console.log("Addition of two Numbers:", add(9, 6));
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+// 1. Ask the first question
+rl.question("Enter Value of a: ", (answer1) => {
+    // 2. Ask the second question inside the first callback
+    rl.question("Enter Value of b: ", (answer2) => {
 
-function Sub(x, y) {
-    return x - y
-}
-console.log("Subtraction of two Numbers:", Sub(a, b));// OR console.log("Subtraction of two Numbers:",Sub(10, 7));
+        // Convert strings to numbers
+        let a = Number(answer1);
+        let b = Number(answer2);
 
-function mul(x, y) {
-    return x * y
-}
 
-console.log("Multiplication of two Numbers: ", mul(a, b));// OR console.log("Multiplication of two Numbers: ",mul(9,5))
+        function add(x, y) {
+            return x + y
+        }
+        console.log("Addition of two Numbers:", add(a, b)); // console.log("Addition of two Numbers:", add(9, 6));
 
-function div(x, y) {
-    return x / y;
-}
-console.log("Division of two numbers: ", div(a, b));// OR console.log("Division of two numbers: ", div(50, 2));
+        function Sub(x, y) {
+            return x - y
+        }
+        console.log("Subtraction of two Numbers:", Sub(a, b));// OR console.log("Subtraction of two Numbers:",Sub(10, 7));
+
+        function mul(x, y) {
+            return x * y
+        }
+
+        console.log("Multiplication of two Numbers: ", mul(a, b));// OR console.log("Multiplication of two Numbers: ",mul(9,5))
+
+        function div(x, y) {
+            return x / y;
+        }
+        console.log("Division of two numbers: ", div(a, b));// OR console.log("Division of two numbers: ", div(50, 2));
+        rl.close(); // ✅ close readline
+    });
+});
