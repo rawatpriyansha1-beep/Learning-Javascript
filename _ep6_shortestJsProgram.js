043ac11e2c3ed187b5b5ae5c35a420b91eabb2b5
Priyansha Rawat <priyansha.rawat@smartcosmos.com>
2026-02-24 14:57:30 +0530

    shortest javascript function

---
+++ _ep6_shortestJsProgram.js
@@ -0,0 +1,5 @@
+// The shortest js program is an empty file
+// window is a globla object it is create along with global execution context
+// js just not run on browsers but in servers and other places so it runs on some engines
+// these engines have the responsibilities ot craete these global objects
+// at global level this==window
