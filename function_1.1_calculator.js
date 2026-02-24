3e2e523bc81b337f34f22c5eaf5f0c6af586fb81
Priyansha Rawat <priyansha.rawat@smartcosmos.com>
2026-02-24 12:19:42 +0530

    changes

---
+++ function_1.1_calculator.js
@@ -0,0 +1,25 @@
+// simple calculator function program 
+
+let a = 8
+let b = 4
+
+function add(x, y) {
+    return x + y
+}
+console.log("Addition of two Numbers:", add(a, b)); // console.log("Addition of two Numbers:", add(9, 6));
+
+function Sub(x, y) {
+    return x - y
+}
+console.log("Subtraction of two Numbers:", Sub(a, b));// OR console.log("Subtraction of two Numbers:",Sub(10, 7));
+
+function mul(x, y) {
+    return x * y
+}
+
+console.log("Multiplication of two Numbers: ", mul(a, b));// OR console.log("Multiplication of two Numbers: ",mul(9,5))
+
+function div(x, y) {
+    return x / y;
+}
+console.log("Division of two numbers: ", div(a, b));// OR console.log("Division of two numbers: ", div(50, 2));
